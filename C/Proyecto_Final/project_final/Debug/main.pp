0c4609f41599d520ea2ae9a19d32b10ddff6b45e
asus <maximiliano.c.correa@gmail.com>
2015-11-08 19:43:56 -0300

    project_final

---
+++ C/Proyecto_Final/project_final/Debug/main.pp
@@ -0,0 +1,9 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+main.obj: /home/asus/SW-TM4C-2.1.1.71/inc/tm4c123gh6pm.h
+
+../main.c: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+/home/asus/SW-TM4C-2.1.1.71/inc/tm4c123gh6pm.h: 
